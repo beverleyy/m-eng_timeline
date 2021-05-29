ec45b477eaf9af73d927fc8ad714c55394811890
beverleyy <62889133+beverleyy@users.noreply.github.com>
2021-05-30 01:05:26 +0800

    Add horizontal scroll

--- js/build.js
+++ js/build.js
@@ -38,3 +38,10 @@ tippy('[title]',{
 	},
 	maxWidth: 300
 });
+
+// Horizontal scroll
+var item = document.getElementsByTagName('main')[0];
+window.addEventListener('wheel', function(e) {
+	if (e.deltaY > 0) item.scrollLeft += 100;
+	else item.scrollLeft -= 100;
+});
